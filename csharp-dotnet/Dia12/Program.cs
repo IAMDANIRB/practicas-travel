8b07477ec60dc666fa4e5d68b3c1fe2fe6e1a1b9
IAMDANIRB <dani.rodriguez.banon@gmail.com>
2026-06-23 12:52:23 +0200

    dia 12: C# basico, tipos, arrays, listas y clases

--- csharp-dotnet/Dia12/Program.cs
+++ csharp-dotnet/Dia12/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Hotel hotel1 = new Hotel();
+hotel1.Nombre = "Hotel Mallorca";
+hotel1.Estrellas = 4;
+hotel1.Precio = 120;
+hotel1.Disponible = true;
+
+Console.WriteLine(hotel1.Describir());
+
+List<Hotel> hoteles = new List<Hotel>();
+hoteles.Add(hotel1);
+
+Hotel hotel2 = new Hotel();
+hotel2.Nombre = "Hotel Ibiza";
+hotel2.Estrellas = 3;
+hotel2.Precio = 80;
+hotel2.Disponible = false;
+hoteles.Add(hotel2);
+
+foreach (Hotel h in hoteles)
+{
+    Console.WriteLine(h.Describir());
+}
+
+// La clase va al final del archivo
+public class Hotel
+{
+    public string Nombre { get; set; }
+    public int Estrellas { get; set; }
+    public double Precio { get; set; }
+    public bool Disponible { get; set; }
+
+    public string Describir()
+    {
+        return Nombre + " - " + Estrellas + " estrellas - " + Precio + "€";
+    }
+}
